d20ebc460c35f5c03055ad070b362e7838744199
asael06 <asael.mb@hotmail.com>
2023-09-03 00:37:33 -0700

    img tag changed into Image element.

--- components/Home/AboutUs.jsx
+++ components/Home/AboutUs.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const AboutUs = () => {
@@ -10,10 +11,11 @@ const AboutUs = () => {
           Ver experiencia
         </Link>
       </div>
-      <img
+      <Image
         className="background"
         src="/images/bg-section.jpg"
         alt="Alumnos estudiando"
+        layout="fill"
       />
     </section>
   );
